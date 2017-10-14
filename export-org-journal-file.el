9b47efa6af9ae006238a47b1c55a696c2d86fed0
Pieter Swinkels <swinkels.pieter@yahoo.com>
2017-10-14 22:42:34 +0200

    Add function to create base filename from date and title

--- export-org-journal-file.el
+++ export-org-journal-file.el
@@ -15,3 +15,15 @@
 (seq-map (lambda (elt) (print (plist-get elt :title)))
          (seq-filter (lambda (elt) (eq 2 (plist-get elt :level)))
                      (org-map-entries 'summarize nil (list "/home/pieter/repos/local/export-org-journal/20170816.org"))))
+
+(defun build-filename (date title extension)
+  (concat date "-" (replace-regexp-in-string " " "-" (downcase title)) extension)
+  )
+
+(build-filename "20170816" "Letting CapsLock be Control and Escape" ".org")
+(build-filename "20170816" "Letting CapsLock be Control and Escape" ".meta")
+
+(org-map-entries 'org-element-at-point nil (list "/home/pieter/repos/local/export-org-journal/20170816.org"))
+
+(seq-filter (lambda (elt) (eq 2 (plist-get elt :level)))
+            (org-map-entries 'summarize nil (list "/home/pieter/repos/local/export-org-journal/20170816.org")))
