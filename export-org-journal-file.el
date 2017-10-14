967a9a6663ecbb1d0e14eef9b30200288fb91c52
Pieter Swinkels <swinkels.pieter@yahoo.com>
2017-10-14 19:35:28 +0200

    Extend summarize to properly extract each journal entry

--- export-org-journal-file.el
+++ export-org-journal-file.el
@@ -1,21 +1,17 @@
 (require 'org)
+(require 'seq)
 
 (defun summarize()
   (let* ((element (org-element-at-point)))
-    (print (nth 1 element)) ;;(org-element-property :heading element))
-    ))
+    (let (properties)
+           (setq properties (plist-put properties :level (org-element-property :level element)))
+           (setq properties (plist-put properties :title (org-element-property :title element)))
+           (setq properties (plist-put properties :content
+                                       (buffer-substring-no-properties
+                                        (org-element-property :contents-begin element)
+                                        (org-element-property :contents-end element))))
+           )))
 
-(defun summarize()
-  (let* ((element (org-element-at-point)))
-    (print (org-element-property :level element))
-    (print (buffer-substring-no-properties
-            (org-element-property :contents-begin element)
-            (org-element-property :contents-end element)))
-    (print (org-element-property :title element))
-    ))
-
-(org-map-entries 'summarize nil (list "/home/pieter/repos/local/export-org-journal/20170816.org"))
-
-(let ((properties '(:title "Hello World!" :date "20170816" :time "10:24" :content "This is a story about the world")))
-  (plist-get properties ':title)
-  )
+(seq-map (lambda (elt) (print (plist-get elt :title)))
+         (seq-filter (lambda (elt) (eq 2 (plist-get elt :level)))
+                     (org-map-entries 'summarize nil (list "/home/pieter/repos/local/export-org-journal/20170816.org"))))
