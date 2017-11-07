81526f0577cecccad44c56d360d4edfbbaebc522
Pieter Swinkels <swinkels.pieter@yahoo.com>
2017-11-07 20:01:39 +0100

    Document functions of oje
    This commit also changes the name of the top-level and private top-level oje
    functions to conform to the Emacs lisp style guide by bbatsov.

--- oje-script.el
+++ oje-script.el
@@ -7,5 +7,5 @@
 (let ((dest-dir "/home/pieter/repos/github.com/nikola-journal/posts")
       (journal-files
        (f-files org-journal-dir (lambda (file) (equal (f-ext file) "org")))))
-  (mapc (lambda (journal-file) (export-journal-entries journal-file dest-dir))
+  (mapc (lambda (journal-file) (oje-export-journal-file journal-file dest-dir))
         journal-files))
--- oje.el
+++ oje.el
@@ -9,12 +9,17 @@
 (require 'f)
 
 (defun get-time(org-element)
+  "Return the value (as a string) of the :TIME property of
+ORG-ELEMENT. If ORG-ELEMENT does not have this property, this
+function returns 00:00."
   (let ((time (org-element-property :TIME org-element)))
     (if (not time)
         (setq time "00:00"))
     time))
 
 (defun summarize()
+  "Return a plist with the information of the
+org-element-at-point."
   (let* ((element (org-element-at-point)))
     (let (properties)
            (setq properties (plist-put properties :level (org-element-property :level element)))
@@ -28,6 +33,9 @@
            )))
 
 (defun extract-date(file-path)
+  "Return the date (as a string) specified by the file name of
+FILE-PATH. This function assumes this file name has format
+YYYYMMDD."
   (let* ((file-name (file-name-base file-path)))
     (concat (substring file-name 0 4) "-"
             (substring file-name 4 6) "-"
@@ -35,15 +43,24 @@
     )
   )
 
-(defun extract-journal-entries(path-to-journal)
-  (let ((date (file-name-base path-to-journal)))
+(defun oje--extract-journal-entries(journal-file-path)
+  "Return a list of plists that each specifies an entry of
+org-journal file JOURNAL-FILE-PATH. The properties of the plist
+are :date and the ones that are extracted by function summarize.
+
+This function only extracts Org section of level 2: it assumes
+that each such section is a separate journal entry."
+  (let ((date (file-name-base journal-file-path)))
     (seq-map (lambda (elt) (plist-put elt :date (extract-date date)))
              (seq-filter (lambda (elt) (eq 2 (plist-get elt :level)))
-                         (org-map-entries 'summarize nil (list path-to-journal))))
+                         (org-map-entries 'summarize nil (list journal-file-path))))
     )
   )
 
-(defun export-meta-info(journal-properties dest-dir)
+(defun oje--export-meta-info(journal-properties dest-dir)
+  "Export JOURNAL-PROPERTIES to a .meta file in DEST-DIR.
+JOURNAL-PROPERTIES is a plist that specifies a single entry of an
+org-journal file."
   (let* ((file-path (build-nikola-file-path journal-properties ".meta" dest-dir))
          (slug (file-name-base file-path))
          (tags (mapconcat 'identity (plist-get journal-properties :tags) ", ")))
@@ -63,7 +80,11 @@
     (save-buffer))
   )
 
-(defun export-blog-content(journal-properties dest-dir)
+(defun oje--export-blog-content(journal-properties dest-dir)
+  "Export JOURNAL-PROPERTIES to a .org file in DEST-DIR.
+JOURNAL-PROPERTIES is a plist that specifies a single entry of an
+org-journal file.
+"
   (let ((file-path (build-nikola-file-path journal-properties ".org" dest-dir)))
     (set-buffer (find-file-noselect file-path))
     (erase-buffer)
@@ -79,10 +100,15 @@
   (concat date "-" (replace-regexp-in-string "[^a-z0-9_.~]" "-" (downcase title)) extension)
   )
 
-(defun export-journal-entries(journal-file-path dest-dir)
-  (let ((journal-entries (extract-journal-entries journal-file-path)))
-    (mapc (lambda (entry) (export-meta-info entry dest-dir)) journal-entries)
-    (mapc (lambda (entry) (export-blog-content entry dest-dir)) journal-entries)
+(defun oje-export-journal-file(journal-file-path dest-dir)
+  "Export all entries of org-journal file JOURNAL-FILE-PATH to
+DEST-DIR. Each entry of the org-journal file is exported to two
+files, namely an .org file that contains the text of the entry
+and a .meta file that contains meta information such as title,
+date and time, tags and slug."
+  (let ((journal-entries (oje--extract-journal-entries journal-file-path)))
+    (mapc (lambda (entry) (oje--export-meta-info entry dest-dir)) journal-entries)
+    (mapc (lambda (entry) (oje--export-blog-content entry dest-dir)) journal-entries)
     nil)
   )
 
